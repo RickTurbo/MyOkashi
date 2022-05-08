905fca4150823991c2cd4f1b54c2721d1e8eaa97
kojima rikuto <kojikoji7765@icloud.com>
2022-05-08 13:19:00 +0900

    Initial Commit

---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 児島　陸斗 on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
