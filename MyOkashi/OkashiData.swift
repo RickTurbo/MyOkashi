5f090db6f9170641bd2ed5a9ad1db1857faccb8d
kojima rikuto <kojikoji7765@icloud.com>
2022-05-08 22:55:14 +0900

    first commit

---
+++ MyOkashi/OkashiData.swift
@@ -0,0 +1,14 @@
+//
+//  OkashiData.swift
+//  MyOkashi
+//
+//  Created by 児島　陸斗 on 2022/05/08.
+//
+
+import Foundation
+
+class OkashiData: ObservableObject {
+    func searchOkashi(keyword: String) async {
+        print(keyword)
+    }
+}
